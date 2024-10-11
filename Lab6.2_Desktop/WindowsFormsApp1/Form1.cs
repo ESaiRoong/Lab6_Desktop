7b8a3e58b7af36995c0922881fe767879a6a5879
Sai <118180477+ESaiRoong@users.noreply.github.com>
2024-10-11 10:55:26 +0700

    Add files via upload

---
+++ Lab6.2_Desktop/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=PC740; database = RestaurantManagement; Integrated Security = true;";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            string query = "SELECT ID, Name, Type FROM Category";
+            sqlCommand.CommandText = query;
+
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            this.DisplayCategory(sqlDataReader);
+
+            sqlConnection.Close();
+        }
+
+        private void DisplayCategory(SqlDataReader sqlDataReader)
+        {
+            lvCategory.Items.Clear();
+
+            while (sqlDataReader.Read())
+            {
+                ListViewItem item = new ListViewItem(sqlDataReader["ID"].ToString());
+
+                lvCategory.Items.Add(item);
+
+                item.SubItems.Add(sqlDataReader["Name"].ToString());
+                item.SubItems.Add(sqlDataReader["Type"].ToString());
+
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=PC740; database = RestaurantManagement; Integrated Security = true;";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandText = "INSERT INTO Category(Name, [Type])" +
+                "VALUES (N'" + txtName.Text + "', " + txtType.Text + ")";
+            sqlConnection.Open();
+
+            int numOfRowsEffected = sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+
+            if (numOfRowsEffected == 1)
+            {
+                MessageBox.Show("Thêm món ăn thành công ");
+                btnLoad.PerformClick();
+
+                txtName.Text = "Name";
+                txtType.Text = "Type";
+            }
+            else
+            {
+                MessageBox.Show("Đã có lỗi xảy ra. Vui lòng thử lại ");
+            }
+        }
+
+        private void lvCategory_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = lvCategory.SelectedItems[0];
+
+            txtID.Text = item.Text;
+            txtName.Text = item.SubItems[1].Text;
+            txtType.Text = item.SubItems[2].Text == "0" ? "Thức uống" : "Đồ ăn";
+
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=PC740; database = RestaurantManagement; Integrated Security = true;";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "UPDATE Category SET Name = N'" + txtName.Text +
+                "', [Type] = " + txtType.Text +
+                " WHERE ID = " + txtID.Text;
+
+            sqlConnection.Open();
+
+            int numOfRowsEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            if (numOfRowsEffected == 1)
+            {
+                ListViewItem item = lvCategory.SelectedItems[0];
+
+                item.SubItems[1].Text = txtName.Text;
+                item.SubItems[2].Text = txtType.Text;
+
+                txtID.Text = "";
+                txtName.Text = "";
+                txtType.Text = "";
+
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+
+
+                MessageBox.Show("Cập nhật thành công ");
+            }
+            else
+            {
+                MessageBox.Show("Đã có lỗi xảy ra. Vui lòng thử lại ");
+            }
+
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=PC740; database = RestaurantManagement; Integrated Security = true;";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandText = "DELETE FROM Category " +
+                "WHERE ID = " + txtID.Text;
+
+            sqlConnection.Open();
+            int numOfRowsEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            if (numOfRowsEffected == 1)
+            {
+                ListViewItem item = lvCategory.SelectedItems[0];
+
+                txtID.Text = "";
+                txtName.Text = "";
+                txtType.Text = "";
+
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+
+
+                MessageBox.Show("Xóa nhóm món ăn thành công ");
+                lvCategory.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Đã có lỗi xảy ra. Vui lòng thử lại ");
+            }
+
+        }
+    }
+}
